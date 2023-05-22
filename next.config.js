5c707be1de8f099bd08e008a441edf48096bea3a
rizkydipahiyat <rizkydipahiyat76@gmail.com>
2023-05-22 13:13:18 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,9 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
-  // env: {
-  //   MONGO_URI:
-  //     "mongodb+srv://rizky123:rizky123@cluster0.t2swy.mongodb.net/blogapp?retryWrites=true&w=majority",
-  //   JWT_SECRET: "password123",
-  //   NEXTAUTH_URL: "https://blogapp-next-six.vercel.app",
-  //   NEXTAUTH_SECRET: "aldjasdsjfb20o3980dn",
-  // },
+  env: {
+    NEXTAUTH_URL: "https://nextblog-lime-zeta.vercel.app",
+  },
   images: {
     remotePatterns: [
       { hostname: "res.cloudinary.com", protocol: "https", port: "" },
